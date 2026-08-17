1a30fbc89ddcb4ba7c12fe0963417150088149d5
ishmeet <ishmeet.singh@tothenew.com>
2026-08-17 22:41:51 +0530

    chore: initial spring boot setup

---
+++ src/test/java/com/shopsphere/inventory/ShopshereInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsphere.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopshereInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
